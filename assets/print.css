8623c94244ddbf6f644a0806b1ee10c3582bc673
rodrigosvolei-creator <rodrigosvolei@gmail.com>
2026-04-13 14:32:18 -0300

    Add files via upload

---
+++ assets/print.css
@@ -0,0 +1,51 @@
+@media print {
+    /* Esconder header, filtros e tabs na impressao */
+    body > div > div > div:first-child {
+        display: none !important;
+    }
+
+    .dash-tabs-content,
+    .tab-container,
+    .tab-parent {
+        border: none !important;
+    }
+
+    /* Esconder botao de imprimir */
+    .print-hide {
+        display: none !important;
+    }
+
+    /* Preservar cores na impressao */
+    body {
+        -webkit-print-color-adjust: exact !important;
+        print-color-adjust: exact !important;
+    }
+
+    #page-oxr {
+        padding: 0 !important;
+    }
+
+    /* Quebra de pagina */
+    #section-mensal {
+        page-break-before: always;
+    }
+
+    #section-graficos {
+        page-break-before: always;
+    }
+
+    /* Tabelas menores */
+    table {
+        font-size: 9px !important;
+    }
+
+    td, th {
+        padding: 3px 5px !important;
+    }
+
+    /* Pagina paisagem */
+    @page {
+        size: A4 landscape;
+        margin: 10mm;
+    }
+}
